2e111743fed22fadfaebc5eb5b62c1407be7df33
LCS-JNS <lucasjnsgorges@gmail.com>
2022-12-12 14:32:37 -0300

    new commit

--- test.java
+++ test.java
@@ -1,5 +1,7 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("Olá Mundo!");
+        System.out.println("Olá Mundo foda!");
+        int n = 0;
+        System.out.println(n);
     }
 }
