1c0c988892ba3822b7f10e00aac3405c072be927
LCS-JNS <lucasjnsgorges@gmail.com>
2023-02-03 18:30:38 -0300

    Fixed shit

--- Api/src/Main.java
+++ Api/src/Main.java
@@ -8,17 +8,17 @@ import javax.json.JsonObject;
 import java.util.Scanner;
 
 public class Main {
-	public String site = "https://economia.awesomeapi.com.br/json/last/";
+	public final String siteBase = "https://economia.awesomeapi.com.br/json/last/";
+	public String siteModify = "https://economia.awesomeapi.com.br/json/last/";
+	public String[] splitedInfo;
+	public String moedas;
 	
-
-	public static void main(String[] args) throws Exception {
-		new Main();
-
-	}
-
 	public Main() throws IOException {
 		setCurrency();
-		String[] splitedInfo = getQuotation();
+	}
+
+	public void print() throws IOException {
+		String[] splitedInfo = this.splitedInfo;
 		for (String s : splitedInfo) {
 			if (!s.equals(splitedInfo[splitedInfo.length - 1])) {
 				System.out.println(s + ",");
@@ -26,11 +26,10 @@ public class Main {
 				System.out.println(s);
 			}
 		}
-		System.out.println();
 	}
 
-	public String[] getQuotation() throws IOException {
-		URL url = new URL(this.site);
+	public void getQuotation() throws IOException {
+		URL url = new URL(this.siteModify);
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 		connection.setRequestMethod("GET");
 		String[] splitedInfo = null;
@@ -41,31 +40,37 @@ public class Main {
 			String info = jsonObject.toString();
 			splitedInfo = info.split(",");
 			in.close();
-
+			this.splitedInfo = splitedInfo;
 		} else if(status == 404){
 			System.out.println("Ambas ou uma das Moedas é inexistente.");
-			new Main();
+			this.siteModify = this.siteBase;
+			setCurrency();
 		} else {
 			System.out.println("Failed to retrieve data: HTTP error code " + status);
 		}
 		connection.disconnect();
-		return splitedInfo;
 	}
 
-	public void setCurrency() {
+	public void setCurrency() throws IOException {
 		Scanner scan = new Scanner(System.in);
 		int aprova = 0;
 		while(aprova == 0) {
 			System.out.print("Informe as duas moedas que deseja usar(Ex: USD-BRL): ");
-			String moeda = scan.nextLine();
-			if(moeda.matches("^[A-Z]{3}-[A-Z]{3}$")) {
-				this.site += moeda;
-				break;
+			this.moedas = scan.next();
+			if(moedas.matches("^[A-Z]{3}-[A-Z]{3}$")) {
+				this.siteModify += moedas;
+				getQuotation();
+				print();
+				System.exit(0);
 			} else {
 				System.out.println("Moedas inválidas.");
 			}
 		}
 		scan.close();
+
 	}
 
+	public static void main(String[] args) throws Exception {
+		new Main();
+	}
 }
